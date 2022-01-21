b8983de7eed3f0d7ad15d09a39aca028a567cb33
mertkaanerdem <mmertkaanerdem@gmail.com>
2022-01-21 23:27:13 +0300

    display feedback for correct or incorrect answers

--- game.js
+++ game.js
@@ -43,7 +43,6 @@ startGame = () => {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
-  console.log(availableQuestions);
   getNewQuestion();
 };
 
@@ -64,7 +63,6 @@ getNewQuestion = () => {
   });
 
   availableQuestions.splice(questionIndex, 1);
-  console.log(availableQuestions);
   acceptingAnswers = true;
 };
 
@@ -75,8 +73,16 @@ choices.forEach((choice) => {
     acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
-    console.log(selectedAnswer);
-    getNewQuestion();
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 });
 
